f8d35772bc601f9be52c134cae94c2c7f718bce8
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-05-29 15:03:41 +0200

    Esercizio Giorno01 - Angular

--- Giorno01/src/app/app.module.ts
+++ Giorno01/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondComponentComponent,
+    ThirdComponentComponent
   ],
   imports: [
     BrowserModule
